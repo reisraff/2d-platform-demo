00cd0f74ff89bc61136f1f36627af220e4e250a6
Rafael Reis <reisraff@gmail.com>
2020-05-12 12:58:34 -0300

    removing imports

---
+++ apple/apple.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _on_got_apple(body):
+	if body.name == 'player':
+		body.got_one_apple()
+		get_node("AnimatedSprite").queue_free()
+		get_node("Area2D").queue_free()
+		get_node("sound").play()
+		get_node("Timer").start()
+
+func _on_music_finished():
+	queue_free()
